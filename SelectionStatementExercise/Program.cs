4e27f86ea59247720d99c7d481e45d784cf46e55
Daniel <dan.r.french@sbcglobal.net>
2023-05-15 12:18:45 -0500

    Completed Selection Statements Exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num = 5;
+
+            Console.WriteLine("Can you guess my favorite number?");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < num)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput > num)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("How did you guess it?!");
+            }
+            Console.ReadLine(); 
         }
     }
 }
